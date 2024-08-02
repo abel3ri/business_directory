f07271e7ee351b68f781e12104d988c25e23a519
abel3ri <abelmerete@yahoo.com>
2024-08-02 23:11:54 +0300

    minor ui changes on home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -37,88 +37,105 @@ class HomePage extends StatelessWidget {
         child: Center(
           child: Column(
             children: [
-              Container(
-                width: Get.width,
-                height: Get.height * 0.25,
-                padding: EdgeInsets.symmetric(
-                  horizontal: 16,
+              Stack(
+                alignment: Alignment.bottomCenter.add(
+                  Alignment(0, 0.45),
                 ),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.primary,
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      spreadRadius: 2,
-                      blurRadius: 100,
-                      offset: Offset(0, 20),
-                      color: Colors.black26,
+                children: [
+                  Container(
+                    width: Get.width,
+                    height: Get.height * 0.3,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 16,
                     ),
-                  ],
-                ),
-                child: Center(
-                  child: Text.rich(
-                    TextSpan(
-                      text: "Discover ",
-                      style:
-                          Theme.of(context).textTheme.headlineMedium!.copyWith(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w900,
-                              ),
-                      children: [
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(8),
+                        topRight: Radius.circular(8),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          spreadRadius: 2,
+                          blurRadius: 100,
+                          offset: Offset(0, 20),
+                          color: Colors.black26,
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: Text.rich(
                         TextSpan(
-                          text:
-                              "Local Businesses and Services at Your Fingertips",
+                          text: "Discover ",
                           style: Theme.of(context)
                               .textTheme
                               .headlineMedium!
                               .copyWith(
                                 color: Colors.white,
+                                fontWeight: FontWeight.w900,
                               ),
-                        )
-                      ],
+                          children: [
+                            TextSpan(
+                              text:
+                                  "Local Businesses and Services at Your Fingertips",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headlineMedium!
+                                  .copyWith(
+                                    color: Colors.white,
+                                  ),
+                            )
+                          ],
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              SizedBox(height: Get.height * 0.02),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                  decoration: BoxDecoration(
-                    color:
-                        Theme.of(context).scaffoldBackgroundColor.lighten(10),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "Search businesses",
-                      suffixIcon: IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.search),
-                      ),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                      decoration: BoxDecoration(
+                        color: Theme.of(context)
+                            .scaffoldBackgroundColor
+                            .lighten(10),
                         borderRadius: BorderRadius.circular(8),
                       ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide.none,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 12,
+                            vertical: 12,
+                          ),
+                          hintText: "Search businesses",
+                          suffixIcon: IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.search),
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          ),
+                          filled: true,
+                          fillColor: Get.isDarkMode
+                              ? Theme.of(context)
+                                  .scaffoldBackgroundColor
+                                  .lighten(20)
+                              : Theme.of(context)
+                                  .scaffoldBackgroundColor
+                                  .darken(20),
+                        ),
                       ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                      ),
-                      filled: true,
-                      fillColor: Get.isDarkMode
-                          ? Theme.of(context)
-                              .scaffoldBackgroundColor
-                              .lighten(20)
-                          : Theme.of(context)
-                              .scaffoldBackgroundColor
-                              .darken(20),
                     ),
                   ),
-                ),
+                ],
               ),
+              // SizedBox(height: Get.height * 0.02),
             ],
           ),
         ),
