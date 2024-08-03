0bedb0c87a347fc14a169c5363175f76eeee133c
abel3ri <abelmerete@yahoo.com>
2024-08-03 16:33:37 +0300

    add reusable button

---
+++ lib/widgets/r_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class RButton extends StatelessWidget {
+  String label;
+  Function()? onPressed;
+  RButton({
+    super.key,
+    required this.label,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton(
+      onPressed: onPressed,
+      child: Text(label),
+      style: ButtonStyle(
+        fixedSize: WidgetStatePropertyAll(Size(100, 16)),
+      ),
+    );
+  }
+}
