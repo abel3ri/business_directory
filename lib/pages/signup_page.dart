439c46ef70e926a555ebc6d16cb710b272a60e12
abel3ri <abelmerete@yahoo.com>
2024-08-03 16:47:16 +0300

    add login and signup pages

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,13 +1,69 @@
+import 'package:business_directory/controllers/text_field_controller.dart';
+import 'package:business_directory/widgets/input_field_row.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SignupPage extends StatelessWidget {
-  const SignupPage({super.key});
+  final textFieldController = Get.put(TextFieldController());
+  SignupPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Sign up page"),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Get.back();
+          },
+          icon: Icon(Icons.arrow_back_ios_new),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Text(
+                  "Ready to explore businesses?",
+                  style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+              ),
+              SizedBox(height: Get.height * 0.04),
+              Row(
+                children: [
+                  RInputField(
+                    controller: textFieldController.firstNameController,
+                    label: "First name",
+                    hintText: "Enter your first name",
+                    keyboardType: TextInputType.name,
+                    textInputAction: TextInputAction.next,
+                  ),
+                  SizedBox(width: Get.width * 0.04),
+                  RInputField(
+                    controller: textFieldController.lastNameController,
+                    label: "Last name",
+                    hintText: "Enter your last name",
+                    keyboardType: TextInputType.name,
+                    textInputAction: TextInputAction.next,
+                  ),
+                ],
+              ),
+              SizedBox(height: Get.height * 0.02),
+              RInputField(
+                controller: textFieldController.emailController,
+                label: "E-mail",
+                hintText: "Enter your email",
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
