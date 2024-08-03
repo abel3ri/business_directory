de94349a65878228c1645a7028c8b5c63e9c5537
abel3ri <abelmerete@yahoo.com>
2024-08-03 12:47:01 +0300

    display category item grid

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:business_directory/widgets/app_drawer.dart';
+import 'package:business_directory/widgets/category_item.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -9,6 +10,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       drawer: AppDrawer(),
       appBar: AppBar(
         leading: Builder(
@@ -135,7 +137,68 @@ class HomePage extends StatelessWidget {
                   ),
                 ],
               ),
-              // SizedBox(height: Get.height * 0.02),
+              SizedBox(
+                height: Get.height * 0.08,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4),
+                child: GridView(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    crossAxisSpacing: 4,
+                  ),
+                  children: [
+                    CategoryItem(
+                      name: "Restaurant",
+                      icon: Icons.restaurant,
+                      color: Colors.red,
+                      onTap: () {},
+                    ),
+                    CategoryItem(
+                      name: "Real Estate",
+                      icon: Icons.home,
+                      color: Colors.amber,
+                      onTap: () {},
+                    ),
+                    CategoryItem(
+                      name: "Tech",
+                      icon: Icons.developer_mode,
+                      color: Colors.blue,
+                      onTap: () {},
+                    ),
+                    CategoryItem(
+                      name: "Shopping",
+                      icon: Icons.shopping_bag,
+                      onTap: () {},
+                    ),
+                    CategoryItem(
+                      name: "Hospital",
+                      icon: Icons.local_hospital,
+                      color: Colors.green,
+                      onTap: () {},
+                    ),
+                    CategoryItem(
+                      name: "Pharmacy",
+                      icon: Icons.medication_liquid_rounded,
+                      color: Colors.lightGreen,
+                      onTap: () {},
+                    ),
+                    CategoryItem(
+                      name: "Shopping",
+                      icon: Icons.shopping_bag,
+                      color: Colors.purple,
+                      onTap: () {},
+                    ),
+                    CategoryItem(
+                      name: "More",
+                      icon: Icons.more_vert_outlined,
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
