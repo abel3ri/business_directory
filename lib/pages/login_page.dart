5fce5389969e335369e69eb45748cc86b979a226
abel3ri <abelmerete@yahoo.com>
2024-08-08 10:18:54 +0300

    add login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,14 +1,95 @@
+import 'package:business_directory/controllers/text_field_controller.dart';
+import 'package:business_directory/utils/form_validator.dart';
+import 'package:business_directory/widgets/form_footer.dart';
+import 'package:business_directory/widgets/input_field_row.dart';
+import 'package:business_directory/widgets/r_button.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  final textFieldController = Get.put(TextFieldController());
+
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text("Login page"),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Get.back();
+          },
+          icon: Icon(Icons.arrow_back_ios_new),
+        ),
+      ),
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Form(
+          key: textFieldController.formKey,
+          child: Obx(
+            () => Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: Text(
+                    "welcomeBack".tr,
+                    style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                ),
+                SizedBox(height: Get.height * 0.04),
+                RInputField(
+                  controller: textFieldController.emailController,
+                  label: "email".tr,
+                  hintText: "enterEmail".tr,
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  validator: FormValidator.emailValidator,
+                ),
+                SizedBox(height: Get.height * 0.02),
+                RInputField(
+                  controller: textFieldController.passwordController,
+                  label: "password".tr,
+                  hintText: "enterPassword".tr,
+                  obscureText: textFieldController.showPassword.value,
+                  keyboardType: TextInputType.visiblePassword,
+                  textInputAction: TextInputAction.next,
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      textFieldController.toggleShowPassword();
+                    },
+                    icon: Icon(
+                      textFieldController.showPassword.value
+                          ? Icons.visibility_off
+                          : Icons.visibility,
+                    ),
+                  ),
+                  validator: FormValidator.passwordValidtor,
+                ),
+                SizedBox(height: Get.height * 0.02),
+                RButton(
+                  label: "signup".tr,
+                  onPressed: () {
+                    if (textFieldController.formKey.currentState!.validate()) {
+                      print("success");
+                    }
+                  },
+                ),
+                FormFooter(
+                  label: "dontHaveAccount".tr,
+                  text: 'signup'.tr,
+                  onPressed: () {
+                    Get.offNamed("signup");
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
