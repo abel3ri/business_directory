8b8909496efae3ac4075da8ee8c08c26f273098a
abel3ri <abelmerete@yahoo.com>
2024-08-16 00:56:59 +0300

    get user position on btn click and add isLoading prop

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
   TextEditingController searchInputController = TextEditingController();
   Rx<int> index = 0.obs;
+  Rx<bool> isLoading = false.obs;
+
+  Rx<Position?> userPosition = Rx<Position?>(null);
 
   void onPageChanged(int i) {
     index.value = i;
+  }
+
+  void toggleIsLoading() {
+    isLoading.value = !isLoading.value;
+  }
+
+  void setUserPosition(Position position) {
+    userPosition.value = position;
   }
 
   @override
