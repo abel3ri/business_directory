b8eed06fbaf2d49c31a02ddf09ddf01a4492e965
abel3ri <abelmerete@yahoo.com>
2024-08-15 13:13:48 +0300

    fix render overflow problem

--- lib/widgets/business_container.dart
+++ lib/widgets/business_container.dart
@@ -9,12 +9,16 @@ class BusinessContainer extends StatelessWidget {
     required this.name,
     required this.location,
     required this.rating,
+    required this.shortDescription,
+    required this.category,
   });
 
   final String imagePath;
   final String name;
   final double rating;
+  final String shortDescription;
   final String location;
+  final String category;
 
   @override
   Widget build(BuildContext context) {
@@ -33,43 +37,61 @@ class BusinessContainer extends StatelessWidget {
           children: [
             Image.asset(
               imagePath,
-              width: 96,
-              height: 96,
+              fit: BoxFit.cover,
             ),
             SizedBox(height: Get.height * 0.01),
             Text(
               name,
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyLarge!
-                  .copyWith(fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: Get.height * 0.01),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Chip(
-                  shape: StadiumBorder(),
-                  backgroundColor: Colors.transparent,
-                  padding: EdgeInsets.all(0),
-                  side: BorderSide.none,
-                  label: Text(
-                    "TECH",
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          fontWeight: FontWeight.bold,
-                        ),
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    fontWeight: FontWeight.bold,
                   ),
-                ),
+            ),
+
+            SizedBox(height: Get.height * 0.01),
+            Text(
+              '"${shortDescription}"',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            Divider(),
+            Chip(
+              shape: StadiumBorder(),
+              backgroundColor: Colors.transparent,
+              padding: EdgeInsets.all(0),
+              side: BorderSide.none,
+              label: Text(
+                category.toUpperCase(),
+                overflow: TextOverflow.ellipsis,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
                 Text(
-                  "${rating}⭐",
+                  "${rating}",
                   style: Theme.of(context).textTheme.bodyMedium!.copyWith(
                         fontWeight: FontWeight.bold,
                       ),
                 ),
+                Icon(
+                  Icons.star_rounded,
+                  size: 16,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
               ],
             ),
+            Divider(),
+            Spacer(),
             Text(
-              '🎈 ${location}',
+              '${location}',
+              overflow: TextOverflow.ellipsis,
               style: Theme.of(context).textTheme.bodySmall,
               textAlign: TextAlign.center,
             ),
