14faed33ea2e88e925cc67b7b89fbf9cf8d8ac12
abel3ri <abelmerete@yahoo.com>
2024-08-16 00:57:38 +0300

    add location update stream to listen user position updates

--- lib/controllers/map_page_controller.dart
+++ lib/controllers/map_page_controller.dart
@@ -1,19 +1,33 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:business_directory/controllers/home_controller.dart';
 
 class MapPageController extends GetxController {
-  Rx<Position?> userPostion = Rx<Position?>(null);
+  Rx<Position?> userPosition = Rx<Position?>(null);
   Rx<bool> isLoading = false.obs;
-
-  LatLng businessLatLng = LatLng(9.020494421702157, 38.75998123861017);
-
+  late StreamSubscription<Position> locationUpdateStream;
   Rx<double> currentZoom = 12.0.obs;
   Rx<LatLng?> currentCenter = Rx<LatLng?>(null);
 
-  void setUserPosition(Position position) {
-    userPostion.value = position;
-    currentCenter.value = LatLng(position.latitude, position.longitude);
+  MapPageController() {
+    userPosition.value = Get.find<HomeController>().userPosition.value;
+    currentCenter.value = LatLng(
+      userPosition.value!.latitude,
+      userPosition.value!.longitude,
+    );
+    locationUpdateStream = Geolocator.getPositionStream(
+      locationSettings: LocationSettings(
+        accuracy: LocationAccuracy.best,
+      ),
+    ).listen((position) {
+      userPosition.value = position;
+      currentCenter.value = LatLng(position.latitude, position.longitude);
+      print(position);
+    });
   }
 
   void toggleIsLoading() {
@@ -26,5 +40,15 @@ class MapPageController extends GetxController {
 
   void decreaseCurrentZoom() {
     currentZoom.value -= 1;
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+    locationUpdateStream.cancel().then((_) {
+      if (kDebugMode) {
+        print("location updates stopped.");
+      }
+    });
   }
 }
