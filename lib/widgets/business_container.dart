92167ee1a130ee45e4ea01871991ecbdd54ced2f
abel3ri <abelmerete@yahoo.com>
2024-08-23 11:42:36 +0300

    redesign business container

--- lib/widgets/business_container.dart
+++ lib/widgets/business_container.dart
@@ -24,91 +24,100 @@ class BusinessContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      clipBehavior: Clip.antiAliasWithSaveLayer,
-      color: Get.isDarkMode
-          ? Theme.of(context).scaffoldBackgroundColor.darken(5)
-          : Theme.of(context).scaffoldBackgroundColor.lighten(10),
-      elevation: 4,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.only(top: 8, left: 12, right: 12),
-        child: Column(
-          children: [
-            Image.asset(
-              imagePath,
-              fit: BoxFit.cover,
-            ),
-            SizedBox(height: Get.height * 0.01),
-            Text(
-              name,
-              overflow: TextOverflow.ellipsis,
-              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                    fontWeight: FontWeight.bold,
-                  ),
-            ),
-
-            SizedBox(height: Get.height * 0.01),
-            Text(
-              '"${shortDescription}"',
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                    fontWeight: FontWeight.bold,
-                  ),
-            ),
-            Divider(),
-            Chip(
-              shape: StadiumBorder(),
-              backgroundColor: Colors.transparent,
-              padding: EdgeInsets.all(0),
-              side: BorderSide.none,
-              label: Text(
-                category.toUpperCase(),
+    return GestureDetector(
+      onTap: () {
+        Get.toNamed("business-details-page");
+      },
+      child: Card(
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        color: Get.isDarkMode
+            ? Theme.of(context).scaffoldBackgroundColor.darken(5)
+            : Theme.of(context).scaffoldBackgroundColor.lighten(10),
+        elevation: 4,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 8, left: 4, right: 4),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 64,
+                width: 64,
+                child: Image.asset(
+                  imagePath,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              SizedBox(height: Get.height * 0.01),
+              Text(
+                name,
                 overflow: TextOverflow.ellipsis,
-                style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
                       fontWeight: FontWeight.bold,
-                      fontSize: 8,
                     ),
               ),
-            ),
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "${rating}",
-                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+              SizedBox(height: Get.height * 0.01),
+              Text(
+                '"${shortDescription}"',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              Divider(),
+              Chip(
+                shape: StadiumBorder(),
+                backgroundColor: Colors.transparent,
+                padding: EdgeInsets.all(0),
+                side: BorderSide.none,
+                label: Text(
+                  category.toUpperCase(),
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
                         fontWeight: FontWeight.bold,
+                        fontSize: 8,
                       ),
                 ),
-                Icon(
-                  Icons.star_rounded,
-                  size: 16,
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-              ],
-            ),
-            Divider(),
-            Spacer(),
-            Text(
-              '${location}',
-              overflow: TextOverflow.ellipsis,
-              style: Theme.of(context).textTheme.bodySmall,
-              textAlign: TextAlign.center,
-            ),
-            // Spacer(),
-            TextButton.icon(
-              label: Text(
-                "showDirection".tr,
-                style: Theme.of(context).textTheme.bodySmall,
               ),
-              onPressed: onPressed,
-              iconAlignment: IconAlignment.end,
-              icon: Icon(Icons.directions),
-            )
-          ],
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "${rating}",
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                  Icon(
+                    Icons.star_rounded,
+                    size: 16,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ],
+              ),
+              Divider(),
+              Spacer(),
+              Text(
+                '${location}',
+                overflow: TextOverflow.ellipsis,
+                style: Theme.of(context).textTheme.bodySmall,
+                textAlign: TextAlign.center,
+              ),
+              // Spacer(),
+              TextButton.icon(
+                label: Text(
+                  "showDirection".tr,
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+                onPressed: onPressed,
+                iconAlignment: IconAlignment.end,
+                icon: Icon(Icons.directions),
+              )
+            ],
+          ),
         ),
       ),
     );
