af8584d67b72fbf637ff0708f860b90759b6d27f
abel3ri <abelmerete@yahoo.com>
2024-08-23 11:34:13 +0300

    add business details page

---
+++ lib/pages/business_details_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BusinessDetailsPage extends StatelessWidget {
+  const BusinessDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Business details page"),
+      ),
+    );
+  }
+}
