821c59f14bfaa93db3cfbc454e678041bdc2e828
abel3ri <abelmerete@yahoo.com>
2024-08-29 22:45:30 +0300

    ui mofification on app error

--- lib/models/app_error.dart
+++ lib/models/app_error.dart
@@ -12,23 +12,23 @@ class AppError {
     Get.closeAllSnackbars();
     Get.showSnackbar(
       GetSnackBar(
-        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
         duration: Duration(seconds: 3),
         animationDuration: Duration(milliseconds: 500),
+        margin: EdgeInsets.symmetric(horizontal: 16),
+        borderRadius: 10,
+        dismissDirection: DismissDirection.horizontal,
+        snackStyle: SnackStyle.FLOATING,
         backgroundColor: Colors.red,
         messageText: Padding(
           padding: EdgeInsets.only(bottom: 8),
-          child: SizedBox(
-            height: Get.height * 0.05,
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                body,
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              body,
+              style: Get.theme.textTheme.bodyLarge!.copyWith(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
               ),
             ),
           ),
