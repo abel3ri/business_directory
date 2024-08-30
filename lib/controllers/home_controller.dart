e275ff6a89d60a52057d3789276d8ec9076f0839
abel3ri <abelmerete@yahoo.com>
2024-08-30 15:10:41 +0300

    fetch user and business on home init

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,3 +1,5 @@
+import 'package:business_directory/controllers/auth_controller.dart';
+import 'package:business_directory/controllers/business_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
@@ -6,8 +8,29 @@ class HomeController extends GetxController {
   TextEditingController searchInputController = TextEditingController();
   Rx<int> index = 0.obs;
   Rx<bool> isLoading = false.obs;
-
   Rx<Position?> userPosition = Rx<Position?>(null);
+  late AuthController authController;
+  late BusinessController businessController;
+
+  @override
+  void onInit() {
+    authController = Get.find<AuthController>();
+
+    authController.getUserData().then((res) {
+      res.fold((l) {
+        l.showError();
+      }, (_) {});
+    });
+
+    businessController = Get.put(BusinessController());
+    businessController.getBusinesses().then((res) {
+      res.fold((l) {
+        l.showError();
+      }, (r) {});
+    });
+
+    super.onInit();
+  }
 
   void onPageChanged(int i) {
     index.value = i;
