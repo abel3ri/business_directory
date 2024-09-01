20e823c87dd3cf316b126036138c8ba466984174
abel3ri <abelmerete@yahoo.com>
2024-09-01 20:36:53 +0300

    platform specific changes:

--- lib/bindings/map_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:business_directory/controllers/map_page_controller.dart';
-import 'package:get/get.dart';
-
-class MapBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => MapPageController());
-  }
-}
