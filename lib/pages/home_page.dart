34e58f165c09778e80fdf78de28af6b7388b46c7
abel3ri <abelmerete@yahoo.com>
2024-09-03 20:06:08 +0300

    refactor home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,22 +1,65 @@
 import 'package:business_directory/controllers/home_controller.dart';
-import 'package:business_directory/pages/profile_page.dart';
 import 'package:business_directory/services/location_service.dart';
 import 'package:business_directory/widgets/app_drawer.dart';
 import 'package:business_directory/widgets/business_container.dart';
 import 'package:business_directory/widgets/category_item_grid.dart';
 import 'package:business_directory/widgets/custom_app_bar.dart';
-import 'package:business_directory/widgets/home_page_search_input.dart';
+import 'package:business_directory/widgets/homepage_search_placeholder.dart';
+import 'package:business_directory/widgets/shimmers/business_shimmer_grid.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
-  final homeController = Get.put(HomeController());
+  final homeController = Get.find<HomeController>();
   HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> items = [
-      RefreshIndicator(
+    print("rebuilt");
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      drawer: AppDrawer(),
+      appBar: CustomAppBar(
+        bottomRenderCondtion: homeController.isLoading,
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              icon: Icon(Icons.sort),
+            );
+          },
+        ),
+        actions: [
+          PopupMenuButton(
+            surfaceTintColor: Colors.transparent,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            position: PopupMenuPosition.under,
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                value: "en_US",
+                child: Text("English"),
+              ),
+              PopupMenuItem(
+                value: "am_ET",
+                child: Text("አማርኛ"),
+              ),
+            ],
+            icon: Icon(Icons.translate_rounded),
+            onSelected: (value) {
+              List<String> splittedLocale = value.split("_");
+              Locale locale = Locale(splittedLocale[0], splittedLocale[1]);
+              Get.updateLocale(locale);
+            },
+          ),
+        ],
+        title: Text("businessDirectory".tr),
+      ),
+      body: RefreshIndicator(
         onRefresh: () => homeController.businessController.getBusinesses(),
         child: SingleChildScrollView(
           physics:
@@ -59,9 +102,7 @@ class HomePage extends StatelessWidget {
                         ),
                       ),
                     ),
-                    Positioned(
-                      child: HomePageSearchInput(),
-                    ),
+                    Positioned(child: HomePageSearchPlaceHolder()),
                   ],
                 ),
                 SizedBox(height: Get.height * 0.06),
@@ -71,125 +112,52 @@ class HomePage extends StatelessWidget {
                 Divider(),
                 SizedBox(height: Get.height * 0.02),
                 Obx(
-                  () => GridView.builder(
-                    physics: NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      mainAxisSpacing: 2,
-                      crossAxisSpacing: 0,
-                      childAspectRatio: 0.55,
-                    ),
-                    itemCount: homeController
-                        .businessController.businesses.value.length,
-                    itemBuilder: (context, index) {
-                      final business = homeController
-                          .businessController.businesses.value[index];
-                      return BusinessContainer(
-                        business: business,
-                        onPressed: () async {
-                          homeController.toggleIsLoading();
-                          final res =
-                              await LocationService().getCurrentPosition();
-                          homeController.toggleIsLoading();
-                          res.fold(
-                            (l) {
-                              l.showError();
-                            },
-                            (r) {
-                              homeController.setUserPosition(r);
-                              Get.toNamed("/map", arguments: {
-                                "businessCoords": business.latLng,
-                              });
+                  () {
+                    return homeController.businessController.isLoading.value
+                        ? BusinessShimmerGrid()
+                        : MasonryGridView.builder(
+                            physics: NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: homeController
+                                .businessController.businesses.value.length,
+                            gridDelegate:
+                                SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                            ),
+                            itemBuilder: (context, index) {
+                              final business = homeController
+                                  .businessController.businesses.value[index];
+                              return BusinessContainer(
+                                tag: "h${business.name}",
+                                business: business,
+                                onShowDirectionTap: () async {
+                                  homeController.toggleIsLoading();
+                                  final res = await LocationService()
+                                      .getCurrentPosition();
+                                  homeController.toggleIsLoading();
+                                  res.fold(
+                                    (l) {
+                                      l.showError();
+                                    },
+                                    (r) {
+                                      homeController.setUserPosition(r);
+                                      Get.toNamed("/map", arguments: {
+                                        "businessCoords": business.latLng,
+                                        "name": business.name,
+                                      });
+                                    },
+                                  );
+                                },
+                              );
                             },
                           );
-                        },
-                      );
-                    },
-                  ),
-                )
+                  },
+                ),
               ],
             ),
           ),
         ),
       ),
-      Center(
-        child: Text("Search"),
-      ),
-      Center(
-        child: Text("Favorites"),
-      ),
-      ProfilePage(),
-    ];
-    return Scaffold(
-      bottomNavigationBar: Obx(
-        () => BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: "Home",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: "Search",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.favorite_rounded),
-              label: "Favorites",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: "Proifle",
-            ),
-          ],
-          currentIndex: homeController.index.value,
-          onTap: (index) {
-            homeController.onPageChanged(index);
-          },
-        ),
-      ),
-      resizeToAvoidBottomInset: false,
-      drawer: AppDrawer(),
-      appBar: CustomAppBar(
-        bottomRenderCondtion: homeController.isLoading,
-        leading: Builder(
-          builder: (context) {
-            return IconButton(
-              onPressed: () {
-                Scaffold.of(context).openDrawer();
-              },
-              icon: Icon(Icons.sort),
-            );
-          },
-        ),
-        actions: [
-          PopupMenuButton(
-            surfaceTintColor: Colors.transparent,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8),
-            ),
-            position: PopupMenuPosition.under,
-            itemBuilder: (context) => [
-              PopupMenuItem(
-                value: "en_US",
-                child: Text("English"),
-              ),
-              PopupMenuItem(
-                value: "am_ET",
-                child: Text("አማርኛ"),
-              ),
-            ],
-            icon: Icon(Icons.translate_rounded),
-            onSelected: (value) {
-              List<String> splittedLocale = value.split("_");
-              Locale locale = Locale(splittedLocale[0], splittedLocale[1]);
-              Get.updateLocale(locale);
-            },
-          ),
-        ],
-        title: Text("businessDirectory".tr),
-      ),
-      body: Obx(() => items[homeController.index.value]),
     );
   }
 }
