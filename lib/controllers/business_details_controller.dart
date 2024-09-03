9f7a3a4c5611a08b3a0dd28c7648abe8cddadd5b
abel3ri <abelmerete@yahoo.com>
2024-09-03 17:34:51 +0300

    add isLoading obs and toggler func

--- lib/controllers/business_details_controller.dart
+++ lib/controllers/business_details_controller.dart
@@ -3,10 +3,13 @@ import 'package:get/get.dart';
 
 class BusinessDetailsController extends GetxController {
   CarouselSliderController controller = CarouselSliderController();
-
+  Rx<bool> isLoading = false.obs;
   Rx<int> activeIndex = 0.obs;
-
   void setCurrentIndex(int index) {
     activeIndex.value = index;
+  }
+
+  void toggleIsLoading() {
+    isLoading.value = !isLoading.value;
   }
 }
